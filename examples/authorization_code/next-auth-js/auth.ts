7245e7ce3e8b97f0121fdf17f4e520adfdf35398
Anthony Gregis <anthony@gregis.dev>
2024-12-18 16:01:27 -0600

    fix(auth): resolve url change

--- examples/authorization_code/next-auth-js/auth.ts
+++ examples/authorization_code/next-auth-js/auth.ts
@@ -1,6 +1,5 @@
-import NextAuth from "next-auth"
-import type { NextAuthConfig } from "next-auth"
-
+import NextAuth from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 
 export const config = {
   theme: {
@@ -12,14 +11,16 @@ export const config = {
       name: "de[id]",
       type: "oauth",
       authorization: {
-        url: "https://verify.de.xyz/oauth/authorize",
-        params: { scope: "wallets:read collections:read dust:read socials:read" }
+        url: "https://de.xyz/oauth/authorize",
+        params: {
+          scope: "wallets:read collections:read dust:read socials:read",
+        },
       },
       checks: ["pkce", "state"],
       token: "https://api.oauth.dustlabs.com/oauth/token",
       userinfo: "https://api.oauth.dustlabs.com/profile",
       client: {
-        token_endpoint_auth_method: "client_secret_post"
+        token_endpoint_auth_method: "client_secret_post",
       },
       clientId: process.env.DEID_CLIENT_ID as string,
       clientSecret: process.env.DEID_CLIENT_SECRET as string,
@@ -28,17 +29,17 @@ export const config = {
           id: response.profile.id,
           name: response.profile.name,
           image: response.profile.imageUrl,
-        }
+        };
       },
-    }
+    },
   ],
   callbacks: {
-    authorized({ request, auth }: { request: any; auth: any}) {
-      const { pathname } = request.nextUrl
-      if (pathname === "/middleware-example") return !!auth
-      return true
+    authorized({ request, auth }: { request: any; auth: any }) {
+      const { pathname } = request.nextUrl;
+      if (pathname === "/middleware-example") return !!auth;
+      return true;
     },
   },
-} satisfies NextAuthConfig
+} satisfies NextAuthConfig;
 
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
